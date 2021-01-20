b85fe8d95c4e8a94e771819633845203af00db76
HarshG-dev <harshaniegomes@gmail.com>
2021-01-20 23:26:39 +0000

    Experimenting with direct use of innerHTML
    Experimenting with direct use of innerHTML see if code become simpler

--- view/js/script.js
+++ view/js/script.js
@@ -19,7 +19,7 @@ function selectItem() {
 // Get Data from backend and display
 function getDataFromBackend(selectedUserID) {
     var callURL = backendAPI + "=" + selectedUserID;
-    var httpRes;    
+    //var httpRes;    
     console.log(callURL);
     
     loadIcon.style.display = "block";
@@ -29,6 +29,7 @@ function getDataFromBackend(selectedUserID) {
     .then(function(respData) {
       userDetails(respData.data[0].userName);
       monthlyData((respData.data), (respData.total_pages));
+      
     })
     .catch(function(error) {
       console.log(error);
@@ -52,43 +53,46 @@ function displayFinalAmount(amount) {
 // Display monthly statement and all the pages
 function monthlyData(statements, pages) {
     let statement = document.querySelector("#monthly-statements");
-    let card, cardBalance, cardDate, node;
+    let card, cardBalance, cardDate, node, cardLocation, cardAddress, cardCity,	cardZipCode;
     
+    // Removing the previously selected items
     while(statement.firstChild) {
         statement.removeChild(statement.lastChild);
     }
     // TODO - Handle pages less than two
     // Sorting the Statement Card in date order
-    statements.sort(function(a, b) { var c = new Date(a.timestamp); var d = new Date(b.timestamp); return d-c; });
+    statements.sort(function(a, b) { 
+        var c = new Date(a.timestamp); 
+        var d = new Date(b.timestamp); 
+        return d-c; 
+    });
+    
     displayFinalAmount(statements[0].amount);
     
     for(let i=0; i<pages; i++) {
         card = document.createElement("div");
         card.classList.add("statement-card");
         
-        cardBalance = document.createElement("p");
-        cardBalance.classList.add("monthly-balance");
-        
-        if (statements[i].txnType == "debit") {
-            cardBalance.classList.add("monthly-debit");
-        } else {
-            cardBalance.classList.add("monthly-credit");
+        cardBalance = document.createElement("div");
+        let statementAmount;
+        if(statements[i].txnType == "debit"){
+            statementAmount = '<p class="monthly-balance monthly-debit">';
+        }else {
+            statementAmount = '<p class="monthly-balance monthly-credit">';
         }
-        
-        node = document.createTextNode(statements[i].amount);
-         
-        cardBalance.appendChild(node);
+        cardBalance.innerHTML = statementAmount + statements[i].amount + '</p>';
         card.appendChild(cardBalance);
         
-        cardDate = document.createElement("p");
-        cardDate.classList.add("month-year");
-        
+        cardDate = document.createElement("div");
         let date = new Date(statements[i].timestamp);
-        const options = {  year: 'numeric', month: 'short'};  
-        node = document.createTextNode(date.toLocaleDateString('en-GB'));
-        
-        cardDate.appendChild(node);
+        cardDate.innerHTML = '<p class="month-year">' + date.toLocaleDateString('en-GB') + '</p>';
         card.appendChild(cardDate);
+        
+        cardLocation = document.createElement("div");
+        cardLocation.classList.add("card-location");
+        let address = '<p>' + statements[i].location.address + '</p>' + '<p>' + statements[i].location.city + '</p>' + '<p>' + statements[i].location.zipCode + '</p>';
+        cardLocation.innerHTML = address;
+        card.appendChild(cardLocation);
         
         statement.appendChild(card);  
     }  
